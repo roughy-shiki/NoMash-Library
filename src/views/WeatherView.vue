4eef395b6082075e2263ff6dbd39411cd726607e
tianfengY <tyua0010@student.monash.edu>
2025-08-09 14:09:43 +0800

    gpt5-2

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -64,7 +64,7 @@ export default {
     },
     iconUrl() {
       return this.weatherData
-        ? `http://openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+        ? `https://openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
         : null;
     },
   },
@@ -76,7 +76,7 @@ export default {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(async (position) => {
           const { latitude, longitude } = position.coords;
-          const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
           await this.fetchWeatherData(url);
         });
       }
@@ -91,7 +91,7 @@ export default {
     },
     async searchByCity() {
       if (!this.city) return;
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(this.city)}&appid=${apikey}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(this.city)}&appid=${apikey}`;
       await this.fetchWeatherData(url);
     }
   }
